b82ac4eca5c53638b775ee61bb9feacfa1851764
Hugo Radulovich <hcradulovich@gmail.com>
2022-11-29 21:45:07 -0300

    avance del proyecto

---
+++ src/TodoApp.jsx
@@ -0,0 +1,18 @@
+import { TodoAdd } from "./components/TodoAdd"
+
+export const TodoApp = () => {
+
+    
+    return (
+
+        <>
+        <h1>TodoApp:10 <small>pendientes: 2</small> </h1>
+        
+
+        <div className="">
+            <TodoAdd/>
+        </div>
+
+        </>
+    )
+}
---
+++ src/components/TodoAdd.jsx
@@ -0,0 +1,28 @@
+import { useForm } from "../hooks/useForm"
+
+
+export const TodoAdd = () => {
+
+    const {description, onIpuntChange, onResetForm} = useForm({
+        description:'',
+    });
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(description)
+        if(description.length<= 1) return;
+
+    }
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <input 
+            type="text" 
+            className=""
+            placeholder="Ingrese tarea a realizar" 
+            onChange = {onIpuntChange}
+            name="description"
+            />
+        </form>
+    )
+}
